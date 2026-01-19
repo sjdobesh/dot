8ec7edd155b314348b50429c58ed515d24c63f60
samantha <samjdobesh@gmail.com>
2026-01-19 12:45:34 -0800

    stashing halfworking init.lua

---
+++ .dotfiles/nvim/.config/init.lua
@@ -0,0 +1,649 @@
+-- init.lua
+-- samantha janes init file
+--    ______    ________  ___
+--   / ___/ |  / /  _/  |/  /
+--   \__ \| | / // // /|_/ /
+--  ___/ /| |/ // // /  / /
+-- /____(_)___/___/_/  /_/
+--
+----------------------------------------------------------------------------(80)
+
+-- OPTIONS -------------------------------------------------------------------80
+
+require("configs.options")
+
+-- KEY MAPS --------------------------------------------------------------------
+
+--require("keymaps")
+
+-- PLUGINS ---------------------------------------------------------------------
+
+-- --require("plugins")
+-- 
+-- 
+-- local plugins = {
+-- 
+--   -- core plugins
+--   { "tpope/vim-surround" },      -- (s)urround command
+--   { "tpope/vim-commentary" },    -- group comments
+--   { "ntpeters/vim-better-whitespace" }, -- whitespace management
+-- 
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     lazy = false,
+--     event = "BufRead",
+--     branch = "main",
+--     build = ":TSUpdate",
+--     ---@class TSConfig
+--     opts = {
+--       -- custom handling of parsers
+--       ensure_installed = {
+--         "bash",
+--         "c",
+--         "css",
+--         "diff",
+--         "glsl",
+--         "go",
+--         "graphql",
+--         "hlsl",
+--         "html",
+--         "javascript",
+--         "jsdoc",
+--         "json",
+--         "json5",
+--         "lua",
+--         "luadoc",
+--         "luap",
+--         "markdown",
+--         "markdown_inline",
+--         "python",
+--         "query",
+--         "regex",
+--         "rust-analyzer",
+--         "toml",
+--         "tsx",
+--         "typescript",
+--         "vim",
+--         "vimdoc",
+--         "yaml",
+--         "ruby",
+--       },
+--     },
+--     config = function(_, opts)
+--       -- install parsers from custom opts.ensure_installed
+--       if opts.ensure_installed and #opts.ensure_installed > 0 then
+--         require("nvim-treesitter").install(opts.ensure_installed)
+--         -- register and start parsers for filetypes
+--         for _, parser in ipairs(opts.ensure_installed) do
+--           local filetypes = parser -- In this case, parser is the filetype/language name
+--           vim.treesitter.language.register(parser, filetypes)
+-- 
+--           vim.api.nvim_create_autocmd({ "FileType" }, {
+--             pattern = filetypes,
+--             callback = function(event)
+--               vim.treesitter.start(event.buf, parser)
+--             end,
+--           })
+--         end
+--       end
+-- 
+--       -- Auto-install and start parsers for any buffer
+--       vim.api.nvim_create_autocmd({ "BufRead" }, {
+--         callback = function(event)
+--           local bufnr = event.buf
+--           local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+-- 
+--           -- Skip if no filetype
+--           if filetype == "" then
+--             return
+--           end
+-- 
+--           -- Check if this filetype is already handled by explicit opts.ensure_installed config
+--           for _, filetypes in pairs(opts.ensure_installed) do
+--             local ft_table = type(filetypes) == "table" and filetypes or { filetypes }
+--             if vim.tbl_contains(ft_table, filetype) then
+--               return -- Already handled above
+--             end
+--           end
+-- 
+--           -- Get parser name based on filetype
+--           local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
+--           if not parser_name then
+--             return
+--           end
+--           -- Try to get existing parser (helpful check if filetype was returned above)
+--           local parser_configs = require("nvim-treesitter.parsers")
+--           if not parser_configs[parser_name] then
+--             return -- Parser not available, skip silently
+--           end
+-- 
+--           local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+-- 
+--           if not parser_installed then
+--             -- If not installed, install parser synchronously
+--             require("nvim-treesitter").install({ parser_name }):wait(30000)
+--           end
+-- 
+--           -- let's check again
+--           parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+-- 
+--           if parser_installed then
+--             -- Start treesitter for this buffer
+--             vim.treesitter.start(bufnr, parser_name)
+--           end
+--         end,
+--       })
+--     end,
+--   },
+--   {
+--     "nvim-treesitter/nvim-treesitter-context",
+--     event = "BufRead",
+--     dependencies = {
+--       "nvim-treesitter/nvim-treesitter",
+--       event = "BufRead",
+--     },
+--     opts = {
+--       multiwindow = true,
+--     },
+--   },
+-- 
+--   -- symbol inspector
+--   {
+--     "hedyhli/outline.nvim",
+--     config = function()
+--       vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+--         { desc = "Toggle Outline" })
+--       require("outline").setup {}
+--     end,
+--   },
+-- 
+--   -- file viewer/editor
+--   {
+--     'stevearc/oil.nvim',
+--     ---@module 'oil'
+--     ---@type oil.SetupOpts
+--     opts = {},
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     lazy = false,
+--   },
+-- 
+--   -- status line
+--   {
+--     'nvim-lualine/lualine.nvim',
+--     dependencies = { 'nvim-tree/nvim-web-devicons' }
+--   },
+-- 
+--   -- dashboard
+--   {
+--     "nvimdev/dashboard-nvim",
+--     event = "VimEnter",
+--     config = function()
+--       local db = require("dashboard")
+--       local custom_header = {
+--         "",
+--         "",
+--         "    .x+=:.                           _            .",
+--         "   z`    ^%                         u            @88>",
+--         "      .   <k           u.    u.    88Nu.   u.    %8P      ..    .     :",
+--         "    .@8Ned8\"         x@88k u@88c. '88888.o888c    .     .888: x888  x888.",
+--         "  .@^%8888\"         ^\"8888\"\"8888\"  ^8888  8888  .@88u  ~`8888~\'888X`?888f`",
+--         " x88:  `)8b.          8888  888R    8888  8888 \'\'888E`   X888  888X \'888>",
+--         " 8888N=*8888          8888  888R    8888  8888   888E    X888  888X \'888>",
+--         "  %8\"    R88          8888  888R    8888  8888   888E    X888  888X \'888>",
+--         "   @8Wou 9%     .     8888  888R   .8888b.888P   888E    X888  888X \'888>",
+--         " .888888P`    .@8c   \"*88*\" 8888\"   ^Y8888*\"\"    888&   \"*88%\"\"*88\" \'888!`",
+--         " `   ^\"F     '%888\"    \"\"   'Y\"       `Y\"        R888\"    `~    \"    `\"`",
+--         "               ^*                                 \"\"",
+--         "",
+--         "",
+--         "",
+--         }
+--       vim.cmd("DisableWhitespace")
+--       -- config
+--       local custom_shortcuts = {
+--         -- open new file
+--         {
+--           icon = "  ",
+--           desc = " new",
+--           key = "n",
+--           action = "ene | startinsert",
+--           group = "DashboardNewFile", -- highlight group
+--         },
+--         -- search
+--         {
+--           icon = " ⟳ ",
+--           desc = " recent",
+--           key = "r",
+--           action = "lua require('telescope.builtin').oldfiles()",
+--           group = "DashboardRecentFiles",
+--         },
+--         {
+--           icon = "  ",
+--           desc = " grep",
+--           key = "g",
+--           action = "lua require('telescope.builtin').live_grep()",
+--           group = "DashboardFindText",
+--         },
+--         -- config files
+--         {
+--           icon = "  ",
+--           desc = " vimconfig",
+--           key = "v",
+--           action = "e ~/.config/nvim/init.lua",
+--           group = "DashboardConfig",
+--         },
+--         {
+--           icon = "  ",
+--           desc = " zconfig",
+--           key = "z",
+--           action = "e ~/.zshrc",
+--           group = "DashboardConfig",
+--         },
+--         {
+--           icon = "  ",
+--           desc = " lconfig",
+--           key = "l",
+--           action = "lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')})",
+--           group = "DashboardConfig",
+--         },
+--         {
+--           icon = " 󱆃 ",
+--           desc = " setup",
+--           key = "s",
+--           action = "e ~/dot/scripts/setup/.bin/setup",
+--           group = "DashboardConfig",
+--         },
+--         {
+--           icon = " 󰿅 ",
+--           desc = " quit",
+--           key = "q",
+--           action = "qa<CR>",
+--           group = "DashboardQuit",
+--         },
+--       }
+-- 
+--       -- display at startup
+--       local custom_footer = function()
+--         if package.loaded["lazy"] then
+--           local stats = require("lazy").stats()
+--           return { "", "", "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. (math.floor(stats.startuptime * 100 + 0.5) / 100) }
+--         else
+--           return { "Press `?` for help" }
+--         end
+--       end
+-- 
+--       db.setup({
+--         theme = "doom", -- "hyper" or "doom"
+--         config = {
+--           header = custom_header,
+--           center = custom_shortcuts,
+--           footer = custom_footer,
+--         },
+--         disable_move = true,
+--       })
+--       vim.cmd([[highlight DashboardNewFile guifg=#50fa7b]])
+--       vim.cmd([[highlight DashboardRecentFiles guifg=#ff79c6]])
+--       vim.cmd([[highlight DashboardFindText guifg=#bd93f9]])
+--       vim.cmd([[highlight DashboardConfig guifg=#ffb86c]])
+--       vim.cmd([[highlight DashboardQuit guifg=#ff5555]])
+-- 
+--     end,
+--     dependencies = { {"nvim-tree/nvim-web-devicons"}}
+--   },
+--   -- colorscheme
+--   {
+--     "sainnhe/sonokai",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       vim.g.sonokai_enable_italic = true
+--       vim.cmd.colorscheme("sonokai")
+--     end
+--   },
+--   -- git gutter
+--   {
+--     "tpope/vim-fugitive",
+--   },
+--   -- branching undo
+--   {
+--     "mbbill/undotree",
+--   },
+--   -- file renderer
+--   {
+--     "OXY2DEV/markview.nvim",
+--     lazy = false,
+--     opts = {
+--       preview = {
+--         filetypes = { "markdown", "latex" },
+--         ignore_buftypes = {},
+--       },
+--     },
+--   },
+--   -- -- telescope fzf
+--   {
+--     "nvim-telescope/telescope.nvim", tag = "v0.2.1",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+--     }
+--   },
+--   -- lsp
+--   { "mason-org/mason-lspconfig.nvim",
+--     opts = {
+--       ensure_installed = mason_lsps,
+--     },
+--     dependencies = {
+--       { "mason-org/mason.nvim", opts = {} },
+--         "neovim/nvim-lspconfig",
+--     },
+--   },
+-- 
+--   {
+--     "folke/lazydev.nvim",
+--     ft = "lua", -- only load on lua files
+--     opts = {
+--       library = {
+--         -- See the configuration section for more details
+--         -- Load luvit types when the `vim.uv` word is found
+--         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+--       },
+--     },
+--   },
+-- 
+--   { -- optional cmp completion source for require statements and module annotations
+--     "hrsh7th/nvim-cmp",
+--     opts = function(_, opts)
+--       opts.sources = opts.sources or {}
+--       table.insert(opts.sources, {
+--         name = "lazydev",
+--         group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+--       })
+--     end,
+--   },
+--   -- blink
+--   {
+--     "saghen/blink.cmp",
+--     dependencies = { "rafamadriz/friendly-snippets" },
+--     version = "1.*",
+--     opts = {
+--       keymap = {
+--         preset = "super-tab",
+--         ["<C-k>"] = { "select_prev", "fallback" },
+--         ["<C-j>"] = { "select_next", "fallback" },
+--         ["<C-s>"] = { "show_signature", "hide_signature" },
+--       },
+--       appearance = { nerd_font_variant = "mono" },
+--       sources = {
+--         default = { "lsp", "path", "snippets", "buffer" },
+--       },
+--       signature = { enabled = true },
+--       fuzzy = { implementation = "prefer_rust_with_warning" },
+--       completion = {
+--         accept = {
+--           auto_brackets = {
+--             enabled = false,
+--           },
+--         },
+--       },
+--     },
+--     opts_extend = { "sources.default" }
+--   },
+-- 
+-- 
+-- }
+-- -- lazy ------------------------------------------------------------------------
+-- 
+-- -- install lazy loader if not available
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable",
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+-- 
+-- -- install plugins
+-- require("lazy").setup(
+--   plugins,
+--   {
+--     checker = {
+--       enabled = true, -- automatically check for updates in the background
+--     },
+--     install = {
+--       colorscheme = { "habamax" },
+--     },
+--   }
+-- )
+-- 
+-- -- plugin settings -------------------------------------------------------------
+-- 
+-- 
+-- --
+-- require("oil").setup({
+--   default_file_explorer = true,
+--   columns = {
+--     "icon",
+--     "permissions",
+--     "size",
+--   },
+--   buf_options = {
+--     buflisted = false,
+--     bufhidden = "hide",
+--   },
+--   win_options = {
+--     wrap = false,
+--     signcolumn = "no",
+--     cursorcolumn = false,
+--     foldcolumn = "0",
+--     spell = false,
+--     list = false,
+--     conceallevel = 3,
+--     concealcursor = "nvic",
+--   },
+--   delete_to_trash = false,
+--   skip_confirm_for_simple_edits = false,
+--   prompt_save_on_select_new_entry = true,
+--   cleanup_delay_ms = 2000,
+--   lsp_file_methods = {
+--     enabled = true,
+--     timeout_ms = 1000,
+--     autosave_changes = false,
+--   },
+--   constrain_cursor = "editable",
+--   watch_for_changes = true,
+--   keymaps = {
+--     ["g?"] = { "actions.show_help", mode = "n" },
+--     ["<CR>"] = "actions.select",
+--     ["<A-s>"] = { "actions.select", opts = { vertical = true } },
+--     ["<A-f>"] = { "actions.select", opts = { tab = true } },
+--     ["<A-p>"] = "actions.preview",
+--     ["<A-c>"] = { "actions.close", mode = "n" },
+--     ["-"] = { "actions.parent", mode = "n" },
+--     ["_"] = { "actions.open_cwd", mode = "n" },
+--     ["`"] = { "actions.cd", mode = "n" },
+--     ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+--     ["gs"] = { "actions.change_sort", mode = "n" },
+--     ["gx"] = "actions.open_external",
+--     ["g."] = { "actions.toggle_hidden", mode = "n" },
+--     ["g\\"] = { "actions.toggle_trash", mode = "n" },
+--     -- ["<A-h>"] = { "actions.select", opts = { horizontal = true } },
+--     -- ["<A-l>"] = "actions.refresh",
+--   },
+--   -- set to false to disable all of the above keymaps
+--   use_default_keymaps = true,
+--   view_options = {
+--     show_hidden = true,
+--     -- defines "hidden" file
+--     is_hidden_file = function(name, bufnr)
+--       local m = name:match("^%.")
+--       return m ~= nil
+--     end,
+--     -- "fast" will turn off sorting for large directories.
+--     natural_order = "fast",
+--     case_insensitive = false,
+--     sort = {
+--       -- see :help oil-columns to see which columns are sortable
+--       { "type", "asc" },
+--       { "name", "asc" },
+--     },
+--   },
+--   git = {
+--     add = function(path)
+--       return true
+--     end,
+--     mv = function(src_path, dest_path)
+--       return true
+--     end,
+--     rm = function(path)
+--       return true
+--     end,
+--   },
+--   float = {
+--     padding = 2,
+--     min_width = { 20, 0.25},
+--     max = { 20, 0.25},
+--     border = nil,
+--     preview_split = "left",
+--     -- config passed to nvim_open_win.
+--     override = function(conf)
+--       return conf
+--     end,
+--   },
+--   preview_win = {
+--     update_on_cursor_moved = true,
+--     preview_method = "fast_scratch",
+--     disable_preview = function(filename)
+--       return false
+--     end,
+--   },
+--   confirmation = {
+--     max_width = 0.9,
+--     min_width = { 40, 0.4 },
+--     width = nil,
+--     max_height = 0.9,
+--     min_height = { 5, 0.1 },
+--     height = nil,
+--     border = nil,
+--     win_options = {
+--       winblend = 0,
+--     },
+--   },
+--   -- configuration for the floating progress window
+--   progress = {
+--     max_width = 0.9,
+--     min_width = { 40, 0.4 },
+--     max_height = { 10, 0.9 },
+--     min_height = { 5, 0.1 },
+--     minimized_border = "none",
+--   },
+--   -- configuration for the floating ssh window
+--   ssh = {
+--     border = nil,
+--   },
+--   -- configuration for the floating keymaps help window
+--   keymaps_help = {
+--     border = nil,
+--   },
+-- })
+-- 
+-- require('lualine').setup {
+--   options = {
+--     icons_enabled = true,
+--     theme = 'auto',
+--     component_separators = { left = '', right = ''},
+--     section_separators = { left = '', right = ''},
+--     disabled_filetypes = {
+--       statusline = {},
+--       winbar = {},
+--     },
+--     ignore_focus = {},
+--     always_divide_middle = true,
+--     always_show_tabline = true,
+--     globalstatus = false,
+--     refresh = {
+--       statusline = 1000,
+--       tabline = 1000,
+--       winbar = 1000,
+--       refresh_time = 16, -- ~60fps
+--       events = {
+--         'WinEnter',
+--         'BufEnter',
+--         'BufWritePost',
+--         'SessionLoadPost',
+--         'FileChangedShellPost',
+--         'VimResized',
+--         'Filetype',
+--         'CursorMoved',
+--         'CursorMovedI',
+--         'ModeChanged',
+--       },
+--     }
+--   },
+--   sections = {
+--     lualine_a = {'mode'},
+--     lualine_b = {'branch', 'diff', 'diagnostics'},
+--     lualine_c = {'filename'},
+--     lualine_x = {'encoding', 'fileformat', 'filetype'},
+--     lualine_y = {'progress'},
+--     lualine_z = {'location'}
+--   },
+--   inactive_sections = {
+--     lualine_a = {},
+--     lualine_b = {},
+--     lualine_c = {'filename'},
+--     lualine_x = {'location'},
+--     lualine_y = {},
+--     lualine_z = {}
+--   },
+--   tabline = {
+--     lualine_a = {'buffers'},
+--     lualine_x = {},
+--     lualine_y = {},
+--     lualine_z = {'tabs'}
+--   },
+--   winbar = {},
+--   inactive_winbar = {},
+--   extensions = {}
+-- }
+-- 
+-- require("telescope").setup({
+--   pickers = {
+--     find_files = {
+--       theme = "ivy",
+--     },
+--     live_grep = {
+--       theme = "ivy",
+--     },
+--   },
+-- })
+-- 
+-- local tele_builtin = require("telescope.builtin")
+-- -- vim.keymap.set({"v", "n"}, "<leader>fs", tele_builtin.grep_string, {})
+-- vim.keymap.set({"v", "n"}, "<leader>fs", function() tele_builtin.grep_string({cwd = "~"}) end, {})
+-- vim.keymap.set("n", "<leader>fa", tele_builtin.find_files, {})
+-- vim.keymap.set("n", "<leader>ff", tele_builtin.git_files, {})
+-- vim.keymap.set("n", "<leader>fg", tele_builtin.live_grep, {})
+-- vim.keymap.set("n", "<leader>fb", tele_builtin.buffers, {})
+-- vim.keymap.set("n", "<leader>fh", tele_builtin.help_tags, {})
+-- 
+-- -- lsp config ------------------------------------------------------------------
+-- 
+-- -- lsp language
+-- local mason_lsps = {
+-- 
+--   -- "lua is handled by lazydev"
+--   "bashls",
+--   "clangd",
+--   "pylsp",
+--   "rust-analyzer",
+-- }
+-- -- Ensure mason and mason-lspconfig are set up first
+-- require("mason").setup({})
+-- require("mason-lspconfig").setup({
+--     ensure_installed = mason_lsps,
+-- })
---
+++ .dotfiles/nvim/.config/lua/configs/options.lua
@@ -0,0 +1,43 @@
+-- s.nvim
+
+-- options -------------------------------------------------------------------80
+
+-- core
+vim.opt.number         = true     -- line numbering
+vim.opt.relativenumber = true     -- relative line numbering
+vim.opt.wrap           = false    -- no wrap
+vim.opt.signcolumn     = "number" -- lets signs and numbers share a collumn
+vim.opt.mouse          = "a"      -- mouse support
+vim.opt.spelllang      = "en-gb"  -- default dictionary
+vim.opt.path:append      "**"     -- better file nav
+
+-- tabs (2 tab width as spaces is default)
+vim.opt.tabstop     = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth  = 2
+vim.opt.expandtab   = true
+vim.opt.smarttab    = true
+vim.opt.autoindent  = true
+
+-- smart searching
+vim.opt.ignorecase = true -- ignore case in searches
+vim.opt.smartcase  = true -- unless its a capital
+vim.opt.gdefault   = true
+
+-- folding
+vim.opt.foldmethod     = "syntax"
+vim.opt.foldcolumn     = "1"
+vim.opt.foldlevelstart = 7
+
+-- terminal colors
+vim.opt.termguicolors = true
+
+-- title
+vim.opt.title = true
+vim.opt.titlestring = "%<%F%=%l/%L - nvim"
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
+-- open new split panes to right and below (as expected)
+vim.opt.splitright = true
+vim.opt.splitbelow = true
